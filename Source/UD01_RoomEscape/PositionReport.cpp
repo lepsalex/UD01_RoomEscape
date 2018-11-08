d6772c4adf40f5aebc0676755c56c644ee033299
Alex <lepsalex@gmail.com>
2018-11-07 22:17:16 -0500

    Get actor name on start with component

---
+++ Source/UD01_RoomEscape/PositionReport.cpp
@@ -0,0 +1,32 @@
+// © Alex Lepsa 2018
+
+#include "PositionReport.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UPositionReport::UPositionReport()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+// Called when the game starts
+void UPositionReport::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FString name = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on: %s"), *name);
+}
+
+
+// Called every frame
+void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
